86587583fc74c59ba1838f0439bac57fc609434a
Matheus Rebello <matheussrb@outlook.com>
2023-08-18 14:22:54 -0300

    Obtendo um usuário por id

--- src/main/java/com/matheusdev/springbootrestapi/resources/UserResource.java
+++ src/main/java/com/matheusdev/springbootrestapi/resources/UserResource.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.jta.UserTransactionAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheusdev.springbootrestapi.dto.UserDTO;
 import com.matheusdev.springbootrestapi.entitties.User;
+import com.matheusdev.springbootrestapi.resources.exception.ResourceExceptionHandler;
 import com.matheusdev.springbootrestapi.services.UserService;
 
 @RestController
@@ -27,6 +29,12 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);	
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 	
 }
--- src/main/java/com/matheusdev/springbootrestapi/services/UserService.java
+++ src/main/java/com/matheusdev/springbootrestapi/services/UserService.java
@@ -1,12 +1,14 @@
 package com.matheusdev.springbootrestapi.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheusdev.springbootrestapi.entitties.User;
 import com.matheusdev.springbootrestapi.repository.UserRepository;
+import com.matheusdev.springbootrestapi.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 } 
