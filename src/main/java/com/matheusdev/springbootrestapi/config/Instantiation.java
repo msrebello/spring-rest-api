2bb023e3d35da18f104d29f5b5774b06b55dc136
Matheus Rebello <matheussrb@outlook.com>
2023-08-19 00:21:19 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/matheusdev/springbootrestapi/config/Instantiation.java
+++ src/main/java/com/matheusdev/springbootrestapi/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.matheusdev.springbootrestapi.config;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.matheusdev.springbootrestapi.entitties.Post;
 import com.matheusdev.springbootrestapi.entitties.User;
+import com.matheusdev.springbootrestapi.repository.PostRepository;
 import com.matheusdev.springbootrestapi.repository.UserRepository;
 
 @Configuration
@@ -14,17 +19,28 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
+
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy").withZone(ZoneId.of("GMT"));
 
+		postRepository.deleteAll();
 		userRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Post post1 = new Post(null, LocalDate.parse("21/03/2018", formatter), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, LocalDate.parse("23/03/2018", formatter), "Bom dia", "Acordei feliz hoje!", maria);
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
 
---
+++ src/main/java/com/matheusdev/springbootrestapi/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.matheusdev.springbootrestapi.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusdev.springbootrestapi.entitties.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
